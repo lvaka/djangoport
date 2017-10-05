600581f8e993fd7a7c6e35a5e42b1dbaf35a5d17
Lvaka <mpstudiosEJShin@gmail.com>
2017-10-04 20:45:47 -0700

    working posting function

--- lvaka/blog/urls.py
+++ lvaka/blog/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$', views.post_new, name='post_new'),
 ]
