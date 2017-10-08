93b4e3fa8de58ebf59cd24bce066e4cc4108fec0
Lvaka <mpstudiosEJShin@gmail.com>
2017-10-07 19:54:02 -0700

    Grrr...

--- lvaka/lvaka/settings.py
+++ lvaka/lvaka/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'lvaka.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangogirls',
+        'NAME': 'lvaka',
         'USER': 'name',
         'PASSWORD': '',
         'HOST': 'localhost',
--- lvaka/lvaka/wsgi.py
+++ lvaka/lvaka/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 from whitenoise.django import DjangoWhiteNoise
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lvaka.settings")
 
 application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
 application = DjangoWhiteNoise(application)
