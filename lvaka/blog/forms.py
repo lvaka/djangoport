835016067c12651b6c52928965317744e94f2ad8
lvaka <mpstudiosEJShin@gmail.com>
2017-10-21 16:19:50 -0700

    Adjustments to Styling on Login and New Posts page as well as aside

--- lvaka/blog/forms.py
+++ lvaka/blog/forms.py
@@ -3,6 +3,11 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
 
+	class Media:
+		css = {
+			'all' : ('style.css')
+		}
+
 	class Meta:
 		model = Post
 		fields = ('title', 'text')
--- lvaka/lvaka/settings.py
+++ lvaka/lvaka/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ggw=-9@zdpg+kmmj%t(as&p2djiwxlp4(-ys#^4u4_2ir^pwp$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['lvakadesigns.com', 'www.lvakadesigns.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'lvakadesigns.com', 'www.lvakadesigns.com']
 
 
 # Application definition
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'lvaka.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 #LOCAL DB
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
-#Dreamhost Setup
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lvaka_mydatabase',
-        'USER': 'lvaka',
-        'PASSWORD': 'holygrail',
-        'HOST': 'mysql.lvakadesigns.com',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#Dreamhost Setup
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'lvaka_mydatabase',
+#        'USER': 'lvaka',
+#        'PASSWORD': 'holygrail',
+#        'HOST': 'mysql.lvakadesigns.com',
+#        'PORT': '3306',
+#    }
+#}
 
 
 # Password validation
